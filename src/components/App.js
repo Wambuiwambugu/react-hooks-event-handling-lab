864db6c9f2ff98b4031c6e490fcfdc306a6d794d
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-05-22 17:27:45 +0300

    Completed assignment

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+// function App() {
+//   return (
+//     <div>
+//         <Keypad />
+//         <EyesOnMe />
+//     </div>
+//   )
+// }
+
+// export default App
